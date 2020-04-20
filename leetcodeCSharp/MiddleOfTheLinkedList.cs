9c1860e18a08fc0e71d88dde34a4d8b1f8613d71
Yuhao Dong <ydong119@gmail.com>
2020-04-20 00:01:33 -0400

    problem 876

---
+++ leetcodeCSharp/MiddleOfTheLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class MiddleOfTheLinkedList
+    {
+        // 876. Middle of the Linked List
+        public ListNode MiddleNode(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            var moveFlag = false;
+
+            var pointer1 = head;
+            var pointer2 = head;
+
+            while (pointer1.next != null)
+            {
+                pointer1 = pointer1.next;
+                if (moveFlag)
+                {
+                    pointer2 = pointer2.next;
+                }
+                moveFlag = !moveFlag;
+            }
+
+            return moveFlag ? pointer2.next : pointer2;
+        }
+    }
+}
