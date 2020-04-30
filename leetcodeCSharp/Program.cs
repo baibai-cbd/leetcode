f080f62501ec8e06a6a1913adb0f262521fc0bae
Yuhao Dong <ydong119@gmail.com>
2020-04-30 11:04:35 -0400

    modify Main method

--- leetcodeCSharp/Program.cs
+++ leetcodeCSharp/Program.cs
@@ -13,9 +13,11 @@ namespace leetcodeCSharp
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //
+            // TODO: Test code can be here
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //
+            Console.ReadKey();
         }
     }
 }
