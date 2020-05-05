1bef459fe475a6fa1a08f987aae66c61ad0d9c43
Yuhao Dong <ydong119@gmail.com>
2020-05-05 00:42:18 -0400

    problem 1009

---
+++ leetcodeCSharp/NumberComplement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class NumberComplement
+    {
+        // 1009. Complement of Base 10 Integer
+        public int FindComplement(int num)
+        {
+            var mask = 1;
+            var input = num;
+            while (input > 1)
+            {
+                input >>= 1;
+                mask = (mask << 1) + 1;
+            }
+            return num ^ mask;
+        }
+    }
+}
