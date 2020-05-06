ca3610bbd6d049375372aa55dc8a3bd305d58a6b
Yuhao Dong <ydong119@gmail.com>
2020-05-06 19:47:53 -0400

    problem 278

---
+++ leetcodeCSharp/FirstBadVersion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class FirstBadVersionSolution
+    {
+        // 278. First Bad Version
+        public int FirstBadVersion(int n)
+        {
+            if (IsBadVersion(1))
+                return 1;
+
+            int lastGood = 1;
+            int firstBad = n;
+            while (firstBad - lastGood > 1)
+            {
+                if (IsBadVersion(firstBad - (firstBad-lastGood)/2))
+                {
+                    firstBad = firstBad - (firstBad - lastGood) / 2;
+                }
+                else
+                {
+                    lastGood = firstBad - (firstBad - lastGood) / 2;
+                }
+            }
+            return firstBad;
+        }
+
+        public bool IsBadVersion(int version)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
