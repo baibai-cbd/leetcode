38a3e1d139a387e70a2029c723f09d1a4856c876
Yuhao Dong <ydong119@gmail.com>
2020-05-05 22:49:24 -0400

    problem 387

---
+++ leetcodeCSharp/FirstUniqueCharacterInAString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class FirstUniqueCharacterInAString
+    {
+        // 387. First Unique Character in a String
+        public int FirstUniqChar(string s)
+        {
+            int[] freqArray = new int[26];
+            int firstIndex = s.Length;
+            foreach (var c in s)
+                freqArray[c - 'a']++;
+            for (int i = 0; i < freqArray.Length; i++)
+            {
+                if (freqArray[i]==1)
+                    firstIndex = Math.Min(firstIndex, s.IndexOf((char)('a' + i)));
+            }
+
+            return firstIndex == s.Length ? -1 : firstIndex;
+        }
+    }
+}
