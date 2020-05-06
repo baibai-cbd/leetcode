e09b415604753a0946a1a23484c75ce09a7a5fcd
Yuhao Dong <ydong119@gmail.com>
2020-05-06 00:07:08 -0400

    problem 122

---
+++ leetcodeCSharp/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class BestTimeToBuyAndSellStockII
+    {
+        // 122. Best Time to Buy and Sell Stock II
+        public int MaxProfit(int[] prices)
+        {
+            int[] profitArray = new int[prices.Length];
+            profitArray[0] = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                profitArray[i] = (prices[i] - prices[i-1] > 0) ? (prices[i] - prices[i-1]) : 0; 
+            }
+            int totalProfit = 0;
+            foreach (var p in profitArray)
+            {
+                totalProfit += p;
+            }
+            return totalProfit;
+        }
+    }
+}
