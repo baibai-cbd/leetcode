99ff80c866f5282c36f6a79dfea1d9a00a16c5c9
Yuhao Dong <ydong119@gmail.com>
2020-05-11 21:52:10 -0400

    problem 733

---
+++ leetcodeCSharp/FloodFill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class FloodFillSolution
+    {
+        // 733. Flood Fill
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            var oldColor = image[sr][sc];
+
+            if (oldColor == newColor)
+                return image;
+
+            FillHelper(image, sr, sc, newColor, oldColor);
+            return image;
+        }
+
+        private void FillHelper(int[][] image, int sr, int sc, int newColor, int oldColor)
+        {
+            if (image[sr][sc] != oldColor)
+                return;
+
+            image[sr][sc] = newColor;
+
+            if (sr + 1 < image.Length)
+                FillHelper(image, sr + 1, sc, newColor, oldColor);
+            if (sr - 1 >= 0)
+                FillHelper(image, sr - 1, sc, newColor, oldColor);
+            if (sc + 1 < image[sr].Length)
+                FillHelper(image, sr, sc + 1, newColor, oldColor);
+            if (sc - 1 >= 0)
+                FillHelper(image, sr, sc - 1, newColor, oldColor);
+        }
+    }
+}
