37d12f67351a5cf04cf11bc9a05d1c979e551981
Yuhao Dong <ydong119@gmail.com>
2020-05-24 10:41:56 -0400

    problem 901

---
+++ leetcodeCSharp/OnlineStockSpan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class OnlineStockSpan
+    {
+        // 901. Online Stock Span
+        public class StockSpanner
+        {
+            readonly Stack<Tuple<int, int>> spanStack;
+
+            public StockSpanner()
+            {
+                spanStack = new Stack<Tuple<int, int>>();
+            }
+
+            public int Next(int price)
+            {
+                var currPair = new Tuple<int, int>(price, 1);
+                if (spanStack.Count == 0 || spanStack.Peek().Item1 > price)
+                {
+                    spanStack.Push(currPair);
+                    return currPair.Item2;
+                }
+
+                while (spanStack.Peek().Item1 <= price)
+                {
+                    var popped = spanStack.Pop();
+                    currPair = new Tuple<int, int>(currPair.Item1, currPair.Item2 + popped.Item2);
+                    if (spanStack.Count == 0)
+                    {
+                        break;
+                    }
+                }
+                spanStack.Push(currPair);
+                return currPair.Item2;
+            }
+        }
+    }
+}
