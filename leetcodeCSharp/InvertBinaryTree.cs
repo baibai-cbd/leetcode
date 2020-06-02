2e3450d6318fc7dae771b1ffe33db18dd7b634a9
Yuhao Dong <ydong119@gmail.com>
2020-06-01 22:26:00 -0400

    problem 226 with C#

---
+++ leetcodeCSharp/InvertBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class InvertBinaryTree
+    {
+        // 226. Invert Binary Tree
+        public TreeNode InvertTree(TreeNode root)
+        {
+            InvertTraverse(root);
+            return root;
+        }
+
+        private void InvertTraverse(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            InvertTraverse(node.left);
+            InvertTraverse(node.right);
+
+            var temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+        }
+    }
+}
