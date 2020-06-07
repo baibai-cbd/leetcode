315da0d08db0250617dc0e536fd20821911e9511
Yuhao Dong <ydong119@gmail.com>
2020-06-07 10:14:11 -0400

    problem 528

---
+++ leetcodeCSharp/RandomPickWithWeight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class RandomPickWithWeight
+    {
+        // 528. Random Pick with Weight
+        // This problem ties to binary search returns negative specific index when not found
+        private double[] probs;
+
+        public RandomPickWithWeight(int[] w)
+        {
+            double sum = 0;
+            probs = new double[w.Length];
+            foreach (var item in w)
+                sum += item;
+            for (int i = 0; i < w.Length; i++)
+            {
+                w[i] += (i == 0) ? 0 : w[i - 1];
+                probs[i] = w[i] / sum;
+            }
+        }
+
+        public int PickIndex()
+        {
+            var rand = new Random();
+            return Math.Abs(Array.BinarySearch(probs, rand.NextDouble())) - 1;
+        }
+    }
+}
