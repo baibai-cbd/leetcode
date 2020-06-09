9eb6610bf441e50a4795081fc2c738c02a6f3280
Yuhao Dong <ydong119@gmail.com>
2020-06-08 21:10:06 -0400

    problem 231

---
+++ leetcodeCSharp/PowerOfTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class PowerOfTwo
+    {
+        // 231. Power of Two
+        public bool IsPowerOfTwo(int n)
+        {
+            int temp = 1;
+            for (int i = 0; i <= 30; i++)
+            {
+                if (n == temp)
+                    return true;
+                temp = temp << 1;
+            }
+            return false;
+        }
+    }
+}
