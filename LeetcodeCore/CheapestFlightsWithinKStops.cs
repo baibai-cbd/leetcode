e6822fec96cc8b8ef68e6dcd0950a454441dbd55
Yuhao Dong <ydong119@gmail.com>
2020-06-14 18:43:02 -0400

    problem 787

---
+++ LeetcodeCore/CheapestFlightsWithinKStops.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class CheapestFlightsWithinKStops
+    {
+        // 787. Cheapest Flights Within K Stops
+        // the dp[i][j] array here defines as:
+        // The min cost from src to j node with at most i edges
+        //
+        // This problem can be solved with Dijkstra algorithm & Bellman Ford algorithm, try those next time
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+        {
+            var dp = new int[K + 2][];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new int[n];
+                Array.Fill(dp[i], int.MaxValue);
+            }
+
+            dp[0][src] = 0;
+            for (int i = 1; i < K + 2; i++)
+            {
+                dp[i][src] = 0;
+                foreach (var f in flights)
+                {
+                    if (dp[i-1][f[0]] != int.MaxValue)
+                        dp[i][f[1]] = Math.Min(dp[i][f[1]], dp[i - 1][f[0]] + f[2]);
+                }
+            }
+
+            return dp[K + 1][dst] == int.MaxValue ? -1 : dp[K + 1][dst];
+        }
+    }
+}
--- LeetcodeCore/Program.cs
+++ LeetcodeCore/Program.cs
@@ -9,10 +9,13 @@ namespace LeetcodeCore
             Console.WriteLine("Hello World!");
             //
             // TODO: Test code can be here
-            var amount = 5;
-            var coins = new int[] { 1, 2, 5 };
-            var cc2 = new CoinChange2();
-            var result = cc2.Change(amount, coins);
+            var n = 3;
+            var flights = new int[][] { new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 0, 2, 500 } };
+            var src = 0;
+            var dst = 2;
+            var K = 0;
+            var testClass = new CheapestFlightsWithinKStops();
+            var result = testClass.FindCheapestPrice(n, flights, src, dst, K);
             Console.WriteLine(result);
             //
             Console.ReadKey();
