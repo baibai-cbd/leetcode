391fd4b9237aad0be3fdb571faeee237205f203d
Yuhao Dong <ydong119@gmail.com>
2020-06-13 22:13:04 -0400

    problem 368

---
+++ leetcodeCSharp/LargestDivisibleSubset.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcodeCSharp
+{
+    public class LargestDivisibleSubsetSolution
+    {
+        // 368. Largest Divisible Subset
+        public IList<int> LargestDivisibleSubset(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return new List<int>();
+
+            Array.Sort(nums);
+            var result = new List<int>();
+            var dp = new int[nums.Length];
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = 1;
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i-1; j >= 0 ; j--)
+                {
+                    if (nums[i] % nums[j] == 0)
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            int maxIndex = 0;
+            for (int i = 1; i < nums.Length; i++)
+                maxIndex = dp[i] > dp[maxIndex] ? i : maxIndex;
+
+            var biggest = nums[maxIndex];
+            var currCount = dp[maxIndex];
+            for (int i = maxIndex; i >= 0; i--)
+            {
+                if (biggest % nums[i] == 0 && dp[i] == currCount)
+                {
+                    result.Add(nums[i]);
+                    currCount--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
