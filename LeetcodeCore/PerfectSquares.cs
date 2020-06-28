4e0de13aece35854cf5d69127d03f90f83ffb0f5
Yuhao Dong <ydong119@gmail.com>
2020-06-27 23:41:19 -0400

    problem 279

---
+++ LeetcodeCore/PerfectSquares.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class PerfectSquares
+    {
+        // 279. Perfect Squares
+        public int NumSquares(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var min = int.MaxValue;
+                int j = 1;
+                while (i - j*j >= 0)
+                {
+                    min = Math.Min(min, dp[i - j * j] + 1);
+                    j++;
+                }
+                dp[i] = min;
+            }
+            return dp[n];
+        }
+    }
+}
