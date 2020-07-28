2d0f299bc21129372c9ff567de491cb2f126354f
Yuhao Dong <ydong119@gmail.com>
2020-07-28 02:25:00 -0400

    Problem 106

---
+++ LeetcodeCore/ConstructBinaryTreeFromInorderAndPostorderTraversal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ConstructBinaryTreeFromInorderAndPostorderTraversal
+    {
+        // 106. Construct Binary Tree from Inorder and Postorder Traversal
+        // try to improve complexity next time
+        public TreeNode BuildTree(int[] inorder, int[] postorder)
+        {
+            if (inorder.Length == 0 || postorder.Length == 0)
+            {
+                return null;
+            }
+            var root = new TreeNode(postorder[postorder.Length - 1]);
+            if (inorder.Length == 1)
+                return root;
+
+            var leftCount = Array.IndexOf(inorder, root.val);
+            var rightCount = inorder.Length - leftCount - 1;
+            root.left = BuildTree(inorder.Take(leftCount).ToArray(), postorder.Take(leftCount).ToArray());
+            root.right = BuildTree(inorder.Skip(leftCount + 1).Take(rightCount).ToArray(), postorder.Skip(leftCount).Take(rightCount).ToArray());
+
+            return root;
+        }
+    }
+}
--- LeetcodeCore/Program.cs
+++ LeetcodeCore/Program.cs
@@ -9,13 +9,12 @@ namespace LeetcodeCore
             Console.WriteLine("Hello World!");
             //
             // TODO: Test code can be here
-            var n = 3;
-            var flights = new int[][] { new int[] { 0, 1, 100 }, new int[] { 1, 2, 100 }, new int[] { 0, 2, 500 } };
-            var src = 0;
-            var dst = 2;
-            var K = 0;
-            var testClass = new CheapestFlightsWithinKStops();
-            var result = testClass.FindCheapestPrice(n, flights, src, dst, K);
+            var inorder = new int[] { 9, 3, 15, 20, 7 };
+            var postorder = new int[] { 9, 15, 7, 20, 3 };
+
+
+            var testClass = new ConstructBinaryTreeFromInorderAndPostorderTraversal();
+            var result = testClass.BuildTree(inorder, postorder);
             Console.WriteLine(result);
             //
             Console.ReadKey();
