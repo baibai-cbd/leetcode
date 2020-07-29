768439bba0d97c0b6b53f2a59c1554de36c3f075
Yuhao Dong <ydong119@gmail.com>
2020-07-29 02:21:18 -0400

    Problem 621

---
+++ LeetcodeCore/TaskScheduler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetcodeCore
+{
+    public class TaskScheduler
+    {
+        // 621. Task Scheduler
+        // Go ahead and understand this
+        public int LeastInterval(char[] tasks, int n)
+        {
+            int[] c = new int[26];
+            foreach (var t in tasks)
+            {
+                c[t - 'A']++;
+            }
+            Array.Sort(c);
+            int i = 25;
+            while (i >= 0 && c[i] == c[25]) i--;
+
+            return Math.Max(tasks.Length, (c[25] - 1) * (n + 1) + 25 - i);
+        }
+    }
+}
