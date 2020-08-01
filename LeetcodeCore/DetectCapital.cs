047db9fc019a5f6f2cb308118fbb4ab54f056402
Yuhao Dong <ydong119@gmail.com>
2020-08-01 11:55:33 -0400

    problem 520

---
+++ LeetcodeCore/DetectCapital.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class DetectCapital
+    {
+        // 520. Detect Capital
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.Length <=1)
+                return true;
+            if (word.ToUpper().Equals(word))
+                return true;
+            if (word.Substring(1).ToLower().Equals(word.Substring(1)))
+                return true;
+
+            return false;
+        }
+    }
+}
