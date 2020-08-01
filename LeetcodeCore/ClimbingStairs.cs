db7a765968503224c2a84e66938c0a85a0ba75a3
Yuhao Dong <ydong119@gmail.com>
2020-08-01 01:07:39 -0400

    Problem 70

---
+++ LeetcodeCore/ClimbingStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ClimbingStairs
+    {
+        // 70. Climbing Stairs
+        public int ClimbStairs(int n)
+        {
+            var arr = new int[n + 1];
+            arr[0] = 1;
+            arr[1] = 1;
+            if (n <= 1) return arr[n];
+            for (int i = 2; i <= n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n];
+        }
+    }
+}
