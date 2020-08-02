354e929ed65c904ef52b19c4be765f5fdb86eb29
Yuhao Dong <ydong119@gmail.com>
2020-08-02 01:49:14 -0400

    Problem 1

---
+++ LeetcodeCore/TwoSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class TwoSumSolution
+    {
+        // 1. Two Sum
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var hs = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var j = target - nums[i];
+                if (hs.Contains(j))
+                    return new int[] { i, Array.IndexOf(nums, j) };
+                hs.Add(nums[i]); // Add after lookup
+            }
+            return null;
+        }
+    }
+}
