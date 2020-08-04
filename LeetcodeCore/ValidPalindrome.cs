c7d8fb1c68c5a1c4291439992b5996fafeea95d7
Yuhao Dong <ydong119@gmail.com>
2020-08-04 00:19:35 -0400

    Problem 125

---
+++ LeetcodeCore/ValidPalindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ValidPalindrome
+    {
+        // 125. Valid Palindrome
+        public bool IsPalindrome(string s)
+        {
+            var st = s.Trim();
+            var stl = st.ToLower();
+            int i = 0;
+            int j = stl.Length - 1;
+
+            while (i < j)
+            {
+                while (!char.IsLetterOrDigit(stl[i]) && i < j)
+                    i++;
+                while (!char.IsLetterOrDigit(stl[j]) && i < j)
+                    j--;
+                if (i == j)
+                    return true;
+                if (stl[i] != stl[j])
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
