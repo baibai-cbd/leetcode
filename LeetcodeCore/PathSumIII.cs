52ff8c1d99a980a88eea0adc1179465c4462ee9a
Yuhao Dong <ydong119@gmail.com>
2020-08-09 00:46:15 -0400

    Problem 437

---
+++ LeetcodeCore/PathSumIII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class PathSumIII
+    {
+        // 437. Path Sum III
+        public int PathSum(TreeNode root, int sum)
+        {
+            if (root == null) return 0;
+            return PathSumFrom(root, sum) + PathSum(root.left, sum) + PathSum(root.right, sum);
+        }
+
+        private int PathSumFrom(TreeNode node, int sum)
+        {
+            if (node == null) return 0;
+            return (node.val == sum ? 1 : 0)
+                + PathSumFrom(node.left, sum - node.val) + PathSumFrom(node.right, sum - node.val);
+        }
+    }
+}
