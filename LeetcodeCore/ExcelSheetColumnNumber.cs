18dcec382911ceecf7a56ba36bdd2b5209ace681
Yuhao Dong <ydong119@gmail.com>
2020-08-11 15:04:27 -0400

    Problem 171

---
+++ LeetcodeCore/ExcelSheetColumnNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ExcelSheetColumnNumber
+    {
+        // 171. Excel Sheet Column Number
+        public int TitleToNumber(String s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                result = result * 26 + (s[i] - 'A' + 1);
+            }
+            return result;
+        }
+    }
+}
