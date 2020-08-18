106feacef746d9480511e9f8c0501b52e6069c67
Yuhao Dong <ydong119@gmail.com>
2020-08-18 01:00:52 -0400

    Problem 1103

---
+++ LeetcodeCore/DistributeCandiesToPeople.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class DistributeCandiesToPeople
+    {
+        // 1103. Distribute Candies to People
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            var index = 0;
+            var currCandy = 1;
+            var remaining = candies;
+            int[] array = new int[num_people];
+
+            while (remaining > 0)
+            {
+                if (remaining >= currCandy)
+                {
+                    array[index] += currCandy;
+                    remaining -= currCandy;
+                }
+                else
+                {
+                    array[index] += remaining;
+                    remaining -= remaining;
+                }
+                if (index == array.Length - 1)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+                currCandy++;
+            }
+            return array;
+        }
+    }
+}
