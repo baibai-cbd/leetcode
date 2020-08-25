681c31b2d4c379095db985e60f2107701c1b032b
Yuhao Dong <ydong119@gmail.com>
2020-08-25 01:45:58 -0400

    Problem 404

---
+++ LeetcodeCore/SumOfLeftLeaves.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class SumOfLeftLeavesSolution
+    {
+        // 404. Sum of Left Leaves
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            var sum = TraverseWithFlag(root, false);
+            return sum;
+        }
+
+        private int TraverseWithFlag(TreeNode node, bool isLeft)
+        {
+            var r = 0;
+            if (node == null)
+                return r;
+
+            if (node.left == null && node.right == null && isLeft)
+                r += node.val;
+
+            r += TraverseWithFlag(node.left, true);
+            r += TraverseWithFlag(node.right, false);
+
+            return r;
+        }
+    }
+}
