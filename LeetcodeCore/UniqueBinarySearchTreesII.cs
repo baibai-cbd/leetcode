8f9bf3feff5cf1ca2694272ac57b8aaa78268ec0
Yuhao Dong <ydong119@gmail.com>
2020-09-11 00:13:38 -0400

    Problem 95

---
+++ LeetcodeCore/UniqueBinarySearchTreesII.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class UniqueBinarySearchTreesII
+    {
+        // 95. Unique Binary Search Trees II
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            if (n == 0)
+                return new List<TreeNode>();
+
+            return GenerateSubTrees(1, n);
+        }
+
+        private IList<TreeNode> GenerateSubTrees(int i, int j)
+        {
+            var list = new List<TreeNode>();
+            if (i > j)
+            {
+                list.Add(null);
+                return list;
+            }
+
+            if (i == j)
+            {
+                list.Add(new TreeNode(i));
+                return list;
+            }
+
+            for (int k = i; k <= j; k++)
+            {
+                var leftNodes = GenerateSubTrees(i, k - 1);
+                var rightNodes = GenerateSubTrees(k + 1, j);
+                foreach (var LNode in leftNodes)
+                {
+                    foreach (var RNode in rightNodes)
+                    {
+                        var currNode = new TreeNode(k);
+                        currNode.left = LNode;
+                        currNode.right = RNode;
+                        list.Add(currNode);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
