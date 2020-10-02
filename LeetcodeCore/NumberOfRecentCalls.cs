d72913222a0dc1a4afe767c35d1302f6cc473fe1
Yuhao Dong <ydong119@gmail.com>
2020-10-02 01:35:03 -0400

    Problem 933

---
+++ LeetcodeCore/NumberOfRecentCalls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class RecentCounter
+    {
+        // 933. Number of Recent Calls
+        private readonly ArrayList _list;
+
+        public RecentCounter()
+        {
+            _list = new ArrayList();
+        }
+
+        public int Ping(int t)
+        {
+            var index = _list.BinarySearch(t - 3000);
+            _list.Add(t);
+            if (index < 0)
+            {
+                return _list.Count - ((index + 1) * -1);
+            }
+            else
+            {
+                return _list.Count - index;
+            }
+        }
+    }
+}
