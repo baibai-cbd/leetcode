f399f73287ecf6841aebd85a8fea1bc1da2cc735
Yuhao Dong <ydong119@gmail.com>
2020-10-14 01:21:40 -0400

    Problem 701

---
+++ LeetcodeCore/InsertIntoABinarySearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class InsertIntoABinarySearchTree
+    {
+        // 701. Insert into a Binary Search Tree
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return new TreeNode(val);
+            }
+            TreeNode node1 = root;
+            bool insertLeft;
+            while (true)
+            {
+                if (node1.val > val)
+                {
+                    if (node1.left != null)
+                    {
+                        node1 = node1.left;
+                        continue;
+                    }
+                    else
+                    {
+                        insertLeft = true;
+                        break;
+                    }
+                }
+                if (node1.val < val)
+                {
+                    if (node1.right != null)
+                    {
+                        node1 = node1.right;
+                        continue;
+                    }
+                    else
+                    {
+                        insertLeft = false;
+                        break;
+                    }
+                }
+            }
+
+            if (insertLeft)
+            {
+                node1.left = new TreeNode(val);
+            }
+            else
+            {
+                node1.right = new TreeNode(val);
+            }
+
+            return root;
+        }
+    }
+}
