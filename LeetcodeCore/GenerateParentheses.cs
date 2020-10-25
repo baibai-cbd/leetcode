16fba70516e927005ffa8c08500f9063f02e670b
Yuhao Dong <ydong119@gmail.com>
2020-10-25 18:22:11 -0400

    Problem 22

---
+++ LeetcodeCore/GenerateParentheses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class GenerateParentheses
+    {
+        // 22. Generate Parentheses
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var results = new List<string>();
+            Backtracking(results, "", n, 0, 0);
+            return results;
+        }
+
+        private void Backtracking(IList<string> results, string s, int n, int left, int right)
+        {
+            if (s.Length == n*2)
+            {
+                results.Add(s);
+                return;
+            }
+
+            if (left < n)
+                Backtracking(results, s + "(", n, left + 1, right);
+            if (right < left)
+                Backtracking(results, s + ")", n, left, right + 1);
+        }
+    }
+}
