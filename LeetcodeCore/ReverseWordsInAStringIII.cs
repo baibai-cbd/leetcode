7c9fbdd254aa3cc53415e852b15306e1f1961792
Yuhao Dong <ydong119@gmail.com>
2020-10-27 18:16:18 -0400

    Problem 557

---
+++ LeetcodeCore/ReverseWordsInAStringIII.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetcodeCore
+{
+    public class ReverseWordsInAStringIII
+    {
+        // 557. Reverse Words in a String III
+        public string ReverseWords(string s)
+        {
+            var words = s.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                var arr = words[i].ToCharArray();
+                Array.Reverse(arr);
+                words[i] = new string(arr);
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
