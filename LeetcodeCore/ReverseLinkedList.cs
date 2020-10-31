6737350300856e34ca4a3d9c52c26a40d06fec9d
Yuhao Dong <ydong119@gmail.com>
2020-10-31 00:54:01 -0400

    Problem 206

---
+++ LeetcodeCore/ReverseLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ReverseLinkedList
+    {
+        // 206. Reverse Linked List
+        // Iterative
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return head;
+            var prev = (ListNode)null;
+            var curr = head;
+
+            while (curr.next != null)
+            {
+                var temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            curr.next = prev;
+            return curr;
+        }
+
+        // Recursive
+        public ListNode ReverseList2(ListNode head)
+        {
+            if (head == null)
+                return head;
+
+            return RecursiveReverse(null, head);
+        }
+
+        private ListNode RecursiveReverse(ListNode prev, ListNode curr)
+        {
+            if (curr.next == null)
+            {
+                curr.next = prev;
+                return curr;
+            }
+
+            var newCurr = curr.next;
+            curr.next = prev;
+            return RecursiveReverse(curr, newCurr);
+        }
+    }
+}
