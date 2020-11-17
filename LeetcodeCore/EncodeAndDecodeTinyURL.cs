89b0b9c667ba7c2823328e3f8a81a57b523e4843
Yuhao Dong <ydong119@gmail.com>
2020-11-17 00:27:33 -0500

    Problem 535

---
+++ LeetcodeCore/EncodeAndDecodeTinyURL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class EncodeAndDecodeTinyURL
+    {
+        // 535. Encode and Decode TinyURL
+        // Not a good question IMO
+        private IDictionary<string, string> _dict = new Dictionary<string, string>();
+        private string _base = @"http://tinyurl.com/";
+
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+            var hash = longUrl.GetHashCode();
+            _dict.Add(hash.ToString(), longUrl);
+            return _base + hash.ToString();
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+            var hashPart = shortUrl.Split('/')[^1];
+            _dict.TryGetValue(hashPart, out string result);
+            return result;
+        }
+    }
+}
