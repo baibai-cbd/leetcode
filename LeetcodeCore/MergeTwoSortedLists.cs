8c976a309356fcfb022fcde3641b92c7678ffbc0
Yuhao Dong <ydong119@gmail.com>
2020-11-19 23:13:02 -0500

    Problem 21

---
+++ LeetcodeCore/MergeTwoSortedLists.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class MergeTwoSortedLists
+    {
+        // 21. Merge Two Sorted Lists
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+
+            var dummy = new ListNode(0);
+            var currHead = dummy;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l1 == null)
+                {
+                    currHead.next = l2;
+                    l2 = l2.next;
+                }
+                else if (l2 == null)
+                {
+                    currHead.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    if (l1.val < l2.val)
+                    {
+                        currHead.next = l1;
+                        l1 = l1.next;
+                    }
+                    else
+                    {
+                        currHead.next = l2;
+                        l2 = l2.next;
+                    }
+                }
+                currHead = currHead.next;
+                currHead.next = null;
+            }
+
+            return dummy.next;
+        }
+    }
+}
