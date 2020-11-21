8a7934547498557713d9c8b31d1e954c5af71338
Yuhao Dong <ydong119@gmail.com>
2020-11-20 23:08:01 -0500

    Problem 98

---
+++ LeetcodeCore/ValidateBinarySearchTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ValidateBinarySearchTree
+    {
+        // 98. Validate Binary Search Tree
+        public bool IsValidBST(TreeNode root)
+        {
+            var list = new List<int>();
+
+            InorderTraverse(root, list);
+
+            if (list.Count <= 1)
+                return true;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] <= list[i-1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void InorderTraverse(TreeNode root, IList<int> list)
+        {
+            if (root == null)
+                return;
+
+            InorderTraverse(root.left, list);
+            list.Add(root.val);
+            InorderTraverse(root.right, list);
+        }
+    }
+}
