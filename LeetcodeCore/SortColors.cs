22a284949cc92222af0eda5e7d63c2fea99a3639
Yuhao Dong <ydong119@gmail.com>
2020-11-24 00:43:07 -0500

    Problem 75

---
+++ LeetcodeCore/SortColors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class SortColorsSolution
+    {
+        // 75. Sort Colors
+        // Two pass method
+        public void SortColors(int[] nums)
+        {
+            var count0 = 0;
+            var count1 = 0;
+            var count2 = 0;
+
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                    count0++;
+                else if (item == 1)
+                    count1++;
+                else
+                    count2++;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i < count0)
+                    nums[i] = 0;
+                else if (i >= count0 && i < count0 + count1)
+                    nums[i] = 1;
+                else
+                    nums[i] = 2;
+            }
+        }
+
+        // TODO: One pass method
+    }
+}
