09b1ddec1c6822be729e8350ed6d66a09649a886
Yuhao Dong <ydong119@gmail.com>
2020-12-01 21:38:28 -0500

    Problem 13

---
+++ LeetcodeCore/RomanToInteger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class RomanToInteger
+    {
+        // 13. Roman to Integer
+        private readonly IDictionary<char, int> _dict = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public int RomanToInt(string s)
+        {
+            var result = 0;
+            var lastLookup = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                _dict.TryGetValue(s[i], out int currLookup);
+                if (currLookup < lastLookup)
+                {
+                    result -= currLookup;
+                }
+                else
+                {
+                    result += currLookup;
+                }
+                lastLookup = currLookup;
+            }
+            return result;
+        }
+    }
+}
