d7964bf42fd2394c592a709fa95090ece51b0108
Yuhao Dong <ydong119@gmail.com>
2020-12-03 00:23:44 -0500

    Problem 979

---
+++ LeetcodeCore/DistributeCoinsInBinaryTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class DistributeCoinsInBinaryTree
+    {
+        // 979. Distribute Coins in Binary Tree
+        public int DistributeCoins(TreeNode root)
+        {
+            var stepsCount = 0;
+            TraverseHelper(root, ref stepsCount);
+            return stepsCount;
+        }
+
+        private int TraverseHelper(TreeNode root, ref int count)
+        {
+            if (root == null)
+                return 0;
+
+            var leftRequest = TraverseHelper(root.left, ref count);
+            var rightRequest = TraverseHelper(root.right, ref count);
+            count += Math.Abs(leftRequest) + Math.Abs(rightRequest);
+            return root.val - 1 + leftRequest + rightRequest;
+        }
+    }
+}
