bed9d07ecd65312d35ef697ba4a7678509d98d8a
Yuhao Dong <ydong119@gmail.com>
2020-12-05 22:40:09 -0500

    Problem 102 one queue solution

--- LeetcodeCore/BinaryTreeLevelOrderTraversal.cs
+++ LeetcodeCore/BinaryTreeLevelOrderTraversal.cs
@@ -7,42 +7,34 @@ namespace LeetcodeCore
     public class BinaryTreeLevelOrderTraversal
     {
         // 102. Binary Tree Level Order Traversal
-        // TODO: Try one queue solution with keeping a record of queue size as the level size
+        // One queue solution with keeping a record of queue size as the level size
         public IList<IList<int>> LevelOrder(TreeNode root)
         {
             var resultList = new List<IList<int>>();
             if (root == null)
                 return resultList;
-            var queue1 = new Queue<TreeNode>();
-            var queue2 = new Queue<TreeNode>();
-            queue1.Enqueue(root);
 
-            while (queue1.Count > 0 || queue2.Count > 0)
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var levelCount = queue.Count; // level count
+            while (levelCount > 0)
             {
-                TraverseLevel(resultList, queue1, queue2);
-                TraverseLevel(resultList, queue2, queue1);
+                var list = new List<int>();
+                for (int i = 0; i < levelCount; i++)
+                {
+                    var node = queue.Dequeue();
+                    list.Add(node.val);
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                }
+                resultList.Add(list);
+                levelCount = queue.Count; // update level count
             }
 
             return resultList;
-        }
-
-        private void TraverseLevel(IList<IList<int>> resultList, Queue<TreeNode> currQueue, Queue<TreeNode> nextQueue)
-        {
-            if (currQueue.Count == 0)
-                return;
-
-            var list = new List<int>();
-            while (currQueue.Count > 0)
-            {
-                var node = currQueue.Dequeue();
-                list.Add(node.val);
-                if (node.left != null)
-                    nextQueue.Enqueue(node.left);
-                if (node.right != null)
-                    nextQueue.Enqueue(node.right);
-            }
-
-            resultList.Add(list);
         }
     }
 }
