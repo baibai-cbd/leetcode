4baa1bb596d47991eeb66cb3c0de22cb94171426
Yuhao Dong <ydong119@gmail.com>
2020-12-10 00:04:15 -0500

    Problem 575

---
+++ LeetcodeCore/DistributeCandies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class DistributeCandiesSolution
+    {
+        // 575. Distribute Candies
+        public int DistributeCandies(int[] candyType)
+        {
+            var hs = new HashSet<int>();
+            var half = candyType.Length / 2;
+
+            for (int i = 0; i < candyType.Length; i++)
+            {
+                hs.Add(candyType[i]);
+                if (hs.Count == half)
+                    break;
+            }
+            return hs.Count;
+        }
+    }
+}
