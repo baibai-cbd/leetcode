d8e38e69c1ffb4cc5d09e1d18cf87ddc2ae654c5
Yuhao Dong <ydong119@gmail.com>
2020-12-12 23:31:45 -0500

    Problem 155 better solution with linkedlist nodes

--- LeetcodeCore/MinStack.cs
+++ LeetcodeCore/MinStack.cs
@@ -6,7 +6,6 @@ namespace LeetcodeCore
 {
     // 155. Min Stack
     // This way is storing old min each time when new min generates
-    // TODO: There's better solution of doing without using library Stack, rather implementing new data structure
     public class MinStack
     {
         private Stack<int> _stack;
@@ -42,6 +41,54 @@ namespace LeetcodeCore
         public int GetMin()
         {
             return _min;
+        }
+    }
+
+    // There's better solution of doing without using library Stack, rather implementing new data structure
+    // Use LinkedList nodes
+    public class MinStack2
+    {
+        private NodeWithMin head;
+
+        public MinStack2()
+        {
+        }
+
+        public void Push(int x)
+        {
+            if (head == null)
+                head = new NodeWithMin(x, x, null);
+            else
+                head = new NodeWithMin(x, Math.Min(x, head.min), head);
+        }
+
+        public void Pop()
+        {
+            head = head.next;
+        }
+
+        public int Top()
+        {
+            return head.val;
+        }
+
+        public int GetMin()
+        {
+            return head.min;
+        }
+    }
+
+    public class NodeWithMin
+    {
+        public int val;
+        public int min;
+        public NodeWithMin next;
+
+        public NodeWithMin(int val, int min, NodeWithMin next)
+        {
+            this.val = val;
+            this.min = min;
+            this.next = next;
         }
     }
 }
