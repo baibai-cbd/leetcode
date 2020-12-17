06fb83ee428ca5d561413984b8f4a133c5cb5b48
Yuhao Dong <ydong119@gmail.com>
2020-12-16 23:01:36 -0500

    Problem 73

---
+++ LeetcodeCore/SetMatrixZeroes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class SetMatrixZeroes
+    {
+        // 73. Set Matrix Zeroes
+        // TODO: Try O(1) solution next time
+        public void SetZeroes(int[][] matrix)
+        {
+            var rows = new bool[matrix.Length];
+            var columns = new bool[matrix[0].Length];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        rows[i] = true;
+                        columns[j] = true;
+                    }
+                }
+            }
+            for (int i = 0; i < rows.Length; i++)
+            {
+                if (rows[i])
+                {
+                    for (int j = 0; j < matrix[0].Length; j++)
+                        matrix[i][j] = 0;
+                }
+            }
+            for (int i = 0; i < columns.Length; i++)
+            {
+                if (columns[i])
+                {
+                    for (int j = 0; j < matrix.Length; j++)
+                        matrix[j][i] = 0;
+                }
+            }
+        }
+    }
+}
