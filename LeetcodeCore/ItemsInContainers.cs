98c2a53806ab3f24cbb7783cdf53a4033f4bbdb6
Yuhao Dong <ydong119@gmail.com>
2020-12-25 21:26:12 -0500

    ItemsInContainers

---
+++ LeetcodeCore/ItemsInContainers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ItemsInContainers
+    {
+        public int[] NumberOfItems(string s, int[] startIndices, int[] endIndices)
+        {
+            int[] results = new int[startIndices.Length];
+            var dict = new Dictionary<Tuple<int,int>, int>();
+            var splittedArr = s.Split('|');
+            var currIndex = splittedArr[0].Length;
+
+            for (int i = 1; i < splittedArr.Length - 1; i++) // don't count in the first/last unclosed intervals, hence starting at 1 and ending with -1
+            {
+                var currClosingIndex = currIndex + splittedArr[i].Length + 1;
+                dict.Add(new Tuple<int, int>(currIndex, currClosingIndex), splittedArr[i].Length);
+                currIndex = currClosingIndex;
+            }
+
+            for (int i = 0; i < startIndices.Length; i++)
+            {
+                var sum = 0;
+                foreach (var kvp in dict)
+                {
+                    if (kvp.Key.Item1 >= startIndices[i] - 1 && kvp.Key.Item2 <= endIndices[i] - 1) // only fully enclosed intervals are counted
+                    {
+                        sum += kvp.Value;
+                    }
+                }
+                results[i] = sum;
+            }
+
+            return results;
+        }
+    }
+}
