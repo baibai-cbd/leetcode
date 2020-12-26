0123ac338ea66ca596fdbaea58cc474ce1ff95fe
Yuhao Dong <ydong119@gmail.com>
2020-12-26 18:29:40 -0500

    TransactionLogs

---
+++ LeetcodeCore/TransactionLogs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class TransactionLogs
+    {
+        // should be a easy problem
+        public string[] ProcessLogFile(string[] logs, int threshold)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (var s in logs)
+            {
+                var arr = s.Split(" ");
+                if (dict.ContainsKey(arr[0]))
+                    dict[arr[0]]++;
+                else
+                    dict.Add(arr[0], 1);
+                if (dict.ContainsKey(arr[1]))
+                    dict[arr[1]]++;
+                else
+                    dict.Add(arr[1], 1);
+                if (arr[0] == arr[1]) // if transaction goes to oneself, count as one
+                    dict[arr[0]]--;
+            }
+
+            var resultArr = dict.Where(kv => kv.Value >= threshold).Select(kv => kv.Key).ToArray();
+            Array.Sort(resultArr);
+            return resultArr;
+        }
+    }
+}
