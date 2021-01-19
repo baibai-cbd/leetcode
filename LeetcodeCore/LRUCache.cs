41300e1cfd4ba49179de817b81750747c5fe4490
Yuhao Dong <ydong119@gmail.com>
2021-01-18 22:44:23 -0500

    Problem 146 LRUCache O(1) solution

--- LeetcodeCore/LRUCache.cs
+++ LeetcodeCore/LRUCache.cs
@@ -7,7 +7,6 @@ namespace LeetcodeCore
     public class LRUCache
     {
         // 146. LRU Cache
-        // TODO: work on O(1) solution next time
         // TODO: try thread-safe implementation next time
         private readonly int _capacity;
         private readonly LinkedList<int> _linkedList;
@@ -52,6 +51,57 @@ namespace LeetcodeCore
                 }
                 _valueDict.Add(key, value);
                 _linkedList.AddFirst(key);
+            }
+        }
+    }
+
+    public class LRUCacheO1
+    {
+        // 146. LRU Cache
+        // O(1) solution
+        private readonly int _capacity;
+        private readonly LinkedList<Tuple<int, int>> _linkedList; // Have to get key from the LinkedListNode, that's why the KV tuple is stored in LinkedListNode
+        private readonly Dictionary<int, LinkedListNode<Tuple<int, int>>> _nodeDict;
+
+        public LRUCacheO1(int capacity)
+        {
+            _capacity = capacity;
+            _linkedList = new LinkedList<Tuple<int,int>>();
+            _nodeDict = new Dictionary<int, LinkedListNode<Tuple<int, int>>>(capacity);
+        }
+
+        public int Get(int key)
+        {
+            if (_nodeDict.TryGetValue(key, out var node))
+            {
+                _linkedList.Remove(node);
+                _linkedList.AddFirst(node);
+                return node.Value.Item2;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void Put(int key, int value)
+        {
+            if (_nodeDict.TryGetValue(key, out var node))
+            {
+                node.Value = new Tuple<int, int>(key, value);
+                _linkedList.Remove(node);
+                _linkedList.AddFirst(node);
+            }
+            else
+            {
+                if (_nodeDict.Count == _capacity)
+                {
+                    _nodeDict.Remove(_linkedList.Last.Value.Item1); // Have to get key from the LinkedListNode, that's why the KV tuple is stored in LinkedListNode
+                    _linkedList.RemoveLast();
+                }
+                var newNode = new LinkedListNode<Tuple<int, int>>(new Tuple<int, int>(key, value));
+                _linkedList.AddFirst(newNode);
+                _nodeDict.Add(key, newNode);
             }
         }
     }
