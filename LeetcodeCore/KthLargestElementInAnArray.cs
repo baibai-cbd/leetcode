7b079f33d6c7d6b19be15274c2cd6c761fa7a28b
Yuhao Dong <ydong119@gmail.com>
2021-01-19 23:27:51 -0500

    Problem 215 KthLargestElementInAnArray

---
+++ LeetcodeCore/KthLargestElementInAnArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class KthLargestElementInAnArray
+    {
+        // 215. Kth Largest Element in an Array
+        // QuickSelect
+        public int FindKthLargest(int[] nums, int k)
+        {
+            return QuickSelect(nums, 0, nums.Length - 1, k);
+        }
+
+        private int QuickSelect(int[] nums, int low, int high, int k)
+        {
+            var partition = Partition(nums, low, high);
+            var count = high - partition + 1;
+
+            if (count == k)
+                return nums[partition];
+            else if (count > k)
+                return QuickSelect(nums, partition + 1, high, k);
+            else
+                return QuickSelect(nums, low, partition - 1, k - count); // (k-count) means deduct the already counted ones
+        }
+
+        private int Partition(int[] nums, int low, int high)
+        {
+            var pivotNum = nums[high];
+            var pivotIndex = low;
+
+            for (int i = low; i < high; i++)
+            {
+                if (nums[i] < pivotNum)
+                {
+                    var temp = nums[i];
+                    nums[i] = nums[pivotIndex];
+                    nums[pivotIndex] = temp;
+                    pivotIndex++;
+                }
+            }
+            var temp2 = nums[pivotIndex];
+            nums[pivotIndex] = nums[high];
+            nums[high] = temp2;
+            return pivotIndex;
+        }
+    }
+}
