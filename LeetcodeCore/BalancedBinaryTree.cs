4de144cda961ac7f320ffeee0f490b2ee5eac7c0
Yuhao Dong <ydong119@gmail.com>
2021-01-31 23:33:34 -0500

    Problem 110 BalancedBinaryTree

---
+++ LeetcodeCore/BalancedBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class BalancedBinaryTree
+    {
+        // 110. Balanced Binary Tree
+        public bool IsBalanced(TreeNode root)
+        {
+            return Traverse(root) != -1;
+        }
+
+        private int Traverse(TreeNode root)
+        {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) return 1;
+
+            var left = Traverse(root.left);
+            var right = Traverse(root.right);
+
+            if (left == -1 || right == -1) 
+                return -1;
+            else if (Math.Abs(left - right) >= 2)
+                return -1;
+            else
+                return Math.Max(left, right) + 1;
+        }
+    }
+}
