5a8d86905234ca1de2d9055112edc8b1a08602bf
Yuhao Dong <ydong119@gmail.com>
2021-02-12 23:28:28 -0500

    Problem 694 NumberOfDistinctIslands

---
+++ LeetcodeCore/NumberOfDistinctIslands.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class NumberOfDistinctIslands
+    {
+        // 694. Number of Distinct Islands
+        public int NumDistinctIslands(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+
+            var pointHashSet = new HashSet<Tuple<int, int>>();
+            var shapeHashSet = new HashSet<string>();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1 && !pointHashSet.Contains(new Tuple<int, int>(i,j)))
+                    {
+                        var stringBuilder = new StringBuilder();
+                        DFSFind(grid, m, n, pointHashSet, i, j, stringBuilder, '*');
+                        if (!shapeHashSet.Contains(stringBuilder.ToString()))
+                            shapeHashSet.Add(stringBuilder.ToString());
+                    }
+                }
+            }
+
+            return shapeHashSet.Count;
+        }
+
+        private void DFSFind(int[][] grid, int m, int n, HashSet<Tuple<int, int>> phs, int i, int j, StringBuilder sb, char direction)
+        {
+            if (i < 0 || j < 0 || i >= m || j >= n)
+                return;
+            if (grid[i][j] == 0)
+                return;
+            if (phs.Contains(new Tuple<int, int>(i, j)))
+                return;
+
+            phs.Add(new Tuple<int, int>(i, j));
+            sb.Append(direction);
+
+            DFSFind(grid, m, n, phs, i, j + 1, sb, 'D');
+            DFSFind(grid, m, n, phs, i + 1, j, sb, 'S');
+            DFSFind(grid, m, n, phs, i, j - 1, sb, 'A');
+            DFSFind(grid, m, n, phs, i - 1, j, sb, 'W');
+
+            sb.Append('/');
+            // need to track back to differentiate between --- and ---
+            //                                             -         -
+        }
+    }
+}
