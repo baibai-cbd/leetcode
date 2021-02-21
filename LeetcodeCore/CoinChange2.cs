943d37fccf2e722cc6e833e54d0ea5170dde529d
Yuhao Dong <ydong119@gmail.com>
2021-02-21 17:25:48 -0500

    Problem 518 CoinChange2 bottom-up DP

--- LeetcodeCore/CoinChange2.cs
+++ LeetcodeCore/CoinChange2.cs
@@ -21,7 +21,7 @@ namespace LeetcodeCore
             var usefulCoins = coins.Where(c => c <= amount).ToArray();
 
             // this dpArr meaning is very tricky,
-            // it's the number of combination of coins to build up current amount by using the current or later index coins(but not previous)
+            // it's the number of combination of coins to build up current amount by using the current and/or later index coins(but not previous)
             dp = new int[usefulCoins.Length, amount+1];
             return ChangeRecursive(amount, usefulCoins, 0);
         }
@@ -42,6 +42,43 @@ namespace LeetcodeCore
 
             dp[index, amount] = sum1 + sum2;
             return dp[index, amount];
+        }
+    }
+
+    public class CoinChange2BottomUp
+    {
+        // 518. Coin Change 2
+        // Unbounded Knapsack problem
+        // Bottom-up dp
+
+        public int Change(int amount, int[] coins)
+        {
+            if (amount == 0)
+                return 1;
+
+            var usefulCoins = coins.Where(c => c <= amount).ToArray();
+
+            // This dpArray meaning is tricky as well
+            // it's the number of combination of coins to build up current amount by using the current and/or previous index coins(but not latter)
+            var dpArr = new int[usefulCoins.Length + 1, amount + 1];
+            dpArr[0, 0] = 1;
+            for (int i = 0; i < usefulCoins.Length; i++)
+            {
+                for (int j = 0; j <= amount; j++)
+                {
+                    if (j >= usefulCoins[i])
+                    {
+                        // (previous coin at this amount) + (this coin at amount-currCoinAmount)
+                        dpArr[i + 1, j] = dpArr[i, j] + dpArr[i + 1, j - usefulCoins[i]];
+                    }
+                    else
+                    {
+                        dpArr[i + 1, j] = dpArr[i, j];
+                    }
+                }
+            }
+
+            return dpArr[usefulCoins.Length, amount];
         }
     }
 }
