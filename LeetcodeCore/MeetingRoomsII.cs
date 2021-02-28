885992a9a6777a2f2553a4555886ce076595c304
Yuhao Dong <ydong119@gmail.com>
2021-02-27 22:15:54 -0500

    Problem 253 MeetingRoomsII more solutions

--- LeetcodeCore/MeetingRoomsII.cs
+++ LeetcodeCore/MeetingRoomsII.cs
@@ -36,5 +36,87 @@ namespace LeetcodeCore
 
             return pqList.Count;
         }
+
+
+
+        // Solution 2
+        // Splitting the start, end events and sort altogether
+        public int MinMeetingRooms2(int[][] intervals)
+        {
+
+            var events = new List<Tuple<int, int>>();
+            foreach (var i in intervals)
+            {
+                events.Add(new Tuple<int, int>(i[0], 1));
+                events.Add(new Tuple<int, int>(i[1], -1));
+            }
+
+            events.Sort(Comparer<Tuple<int, int>>.Create((a, b) =>
+            {
+                if (a.Item1.CompareTo(b.Item1) != 0)
+                    return a.Item1.CompareTo(b.Item1);
+                else
+                    return a.Item2.CompareTo(b.Item2);
+            }));
+
+            var stack = new Stack<Tuple<int, int>>();
+            var max = 0;
+            foreach (var e in events)
+            {
+                stack.Push(e);
+                if (stack.Peek().Item2 == -1)
+                {
+                    stack.Pop();
+                    stack.Pop();
+                }
+                max = Math.Max(max, stack.Count);
+            }
+
+            return max;
+        }
+
+
+        // Solution 3
+        // Splitting the start, end events and using 2 arrays for sort, very skillful solution
+        public int MinMeetingRooms3(int[][] intervals)
+        {
+            int[] starts = new int[intervals.Length];
+            int[] ends = new int[intervals.Length];
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                starts[i] = intervals[i][0];
+                ends[i] = intervals[i][1];
+            }
+            Array.Sort(starts);
+            Array.Sort(ends);
+            int rooms = 0;
+            int endsItr = 0;
+            for (int i = 0; i < starts.Length; i++)
+            {
+                if (starts[i] < ends[endsItr])
+                    rooms++;
+                else
+                    endsItr++;
+            }
+            return rooms;
+        }
+
+
+        // Solution 4
+        // Java with TreeMap
+        //public int minMeetingRooms(Interval[] intervals)
+        //{
+        //    Map<Integer, Integer> map = new TreeMap<>();
+        //    for (Interval itl : intervals)
+        //    {
+        //        map.put(itl.start, map.getOrDefault(itl.start, 0) + 1);
+        //        map.put(itl.end, map.getOrDefault(itl.end, 0) - 1);
+        //    }
+        //    int room = 0, k = 0;
+        //    for (int v : map.values())
+        //        k = Math.max(k, room += v);
+        //
+        //    return k;
+        //}
     }
 }
