689d8830d35b844c2b2bef20a7a3b2b6bd0dd133
Yuhao Dong <ydong119@gmail.com>
2021-02-28 19:19:00 -0500

    Problem 77 Combinations with backtracking

--- LeetcodeCore/Combinations.cs
+++ LeetcodeCore/Combinations.cs
@@ -31,5 +31,33 @@ namespace LeetcodeCore
             DFS(results, currList, n, k, index + 1);
             currList.RemoveAt(currList.Count - 1);
         }
+
+
+        // Backtracking
+        // backtracking usually use a loop over the next step options, 
+        // add the element to the current result set, then recursively call,
+        // remove the current element after the recursive call returns
+        public IList<IList<int>> Combine2(int n, int k)
+        {
+            var results = new List<IList<int>>();
+            Backtrack(results, new List<int>(), n, k, 1);
+            return results;
+        }
+
+        private void Backtrack(IList<IList<int>> results, IList<int> currList, int n, int k, int index)
+        {
+            if (currList.Count == k)
+            {
+                results.Add(new List<int>(currList));
+                return;
+            }
+
+            for (int i = index; i <= n; i++)
+            {
+                currList.Add(i);
+                Backtrack(results, currList, n, k, i + 1);
+                currList.RemoveAt(currList.Count - 1);
+            }
+        }
     }
 }
