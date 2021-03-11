cc8e4f7423eb7f8f5211358deb6a4609d4477833
Yuhao Dong <ydong119@gmail.com>
2021-03-10 23:46:40 -0500

    A PriorityQueue Implementation

---
+++ LeetcodeCore/PriorityQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    // My own implementation of PriorityQueue with generic capability
+    public class PriorityQueue<T>
+    {
+        private IList<T> _list;
+        private IComparer<T> _comparer;
+
+        public PriorityQueue()
+        {
+            _list = new List<T>();
+            _list.Add(default);
+            _comparer = Comparer<T>.Default;
+        }
+
+        public PriorityQueue(IComparer<T> comparer)
+        {
+            _list = new List<T>();
+            _list.Add(default);
+            _comparer = comparer;
+        }
+
+        public int Count { get { return _list.Count - 1; } }
+
+        public void Push(T value)
+        {
+            var newIndex = _list.Count;
+            _list.Add(value);
+
+            while (newIndex > 1 && _comparer.Compare(_list[newIndex], _list[newIndex/2]) < 0)
+            {
+                Swap(newIndex / 2, newIndex);
+                newIndex /= 2;
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            var temp = _list[i];
+            _list[i] = _list[j];
+            _list[j] = temp;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count < 2)
+                throw new InvalidOperationException();
+
+            return _list[1];
+        }
+
+        public T Pop()
+        {
+            if (_list.Count < 2)
+                throw new InvalidOperationException();
+
+            T result = _list[1];
+            _list[1] = _list[^1];
+            _list.RemoveAt(_list.Count - 1);
+            Heapify(1);
+
+            return result;
+        }
+
+        private void Heapify(int index)
+        {
+            var smallest = index;
+            var left = index * 2;
+            var right = index * 2 + 1;
+
+            if (left < _list.Count && _comparer.Compare(_list[left], _list[smallest]) < 0)
+            {
+                smallest = left;
+            }
+            if (right < _list.Count && _comparer.Compare(_list[right], _list[smallest]) < 0)
+            {
+                smallest = right;
+            }
+            if (smallest != index)
+            {
+                Swap(index, smallest);
+                Heapify(smallest);
+            }
+        }
+    }
+}
