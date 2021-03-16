de0cd8dd79ae60da70248e5d25851655233e48d0
Yuhao Dong <ydong119@gmail.com>
2021-03-15 23:04:43 -0400

    Problem 121 BestTimeToBuyAndSellStock

---
+++ LeetcodeCore/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        // 121. Best Time to Buy and Sell Stock
+        public int MaxProfit(int[] prices)
+        {
+            var profitArr = new int[prices.Length];
+            profitArr[0] = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                profitArr[i] = prices[i] - prices[i - 1];
+            }
+
+            var max = 0;
+            var currProfit = 0;
+            for (int j = 0; j < prices.Length; j++)
+            {
+                currProfit = Math.Max(currProfit + profitArr[j], 0);
+                max = Math.Max(currProfit, max);
+            }
+
+            return max;
+        }
+    }
+}
