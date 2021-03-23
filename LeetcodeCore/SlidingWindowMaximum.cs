b8de48c724be1a9e3312ee3d1bd25568434bb914
Yuhao Dong <ydong119@gmail.com>
2021-03-22 23:19:34 -0400

    Problem 239 SlidingWindowMaximum with MonotonicQueue solution

--- LeetcodeCore/SlidingWindowMaximum.cs
+++ LeetcodeCore/SlidingWindowMaximum.cs
@@ -7,7 +7,7 @@ namespace LeetcodeCore
     public class SlidingWindowMaximum
     {
         // 239. Sliding Window Maximum
-        // TODO: Use Monotonic Queue for thie problem next time
+        // This PQ solution is pretty bad, look below for better solutoin(Monotonic Queue)
         public int[] MaxSlidingWindow(int[] nums, int k)
         {
             var i = 0;
@@ -37,6 +37,61 @@ namespace LeetcodeCore
             }
 
             return resultList.ToArray();
+        }
+
+        // Monotonic Queue
+        // Very nice solution
+        public int[] MaxSlidingWindow2(int[] nums, int k)
+        {
+            var i = 0;
+            var j = k - 1;
+            var resultList = new List<int>();
+            var queue = new LinkedList<(int, int)>();
+
+            for (int x = i; x <= j; x++)
+            {
+                MonotonicPush(queue, nums[x]);
+            }
+
+            resultList.Add(queue.First.Value.Item1);
+
+            for (int y = j + 1; y < nums.Length; y++)
+            {
+                MonotonicPop(queue);
+                MonotonicPush(queue, nums[y]);
+                resultList.Add(queue.First.Value.Item1);
+            }
+
+            return resultList.ToArray();
+        }
+
+        private void MonotonicPush(LinkedList<(int, int)> linkedList, int value)
+        {
+            var newNode = new LinkedListNode<(int, int)>((value, 1));
+            while (linkedList.Count > 0 && linkedList.Last.Value.Item1 < newNode.Value.Item1)
+            {
+                newNode.Value = (newNode.Value.Item1, newNode.Value.Item2 + linkedList.Last.Value.Item2);
+                linkedList.RemoveLast();
+            }
+            linkedList.AddLast(newNode);
+        }
+
+        private int MonotonicPop(LinkedList<(int, int)> linkedList)
+        {
+            if (linkedList.Count == 0)
+                throw new InvalidOperationException();
+
+            if (linkedList.First.Value.Item2 == 1)
+            {
+                var result = linkedList.First;
+                linkedList.RemoveFirst();
+                return result.Value.Item1;
+            }
+            else
+            {
+                linkedList.First.Value = (linkedList.First.Value.Item1, linkedList.First.Value.Item2 - 1);
+                return linkedList.First.Value.Item1;
+            }
         }
     }
 }
