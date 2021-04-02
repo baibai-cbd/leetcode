e63eafc125dd6382b6671e374e8bf87b052f63ab
Yuhao Dong <ydong119@gmail.com>
2021-04-02 00:11:24 -0400

    Problem 68 TextJustification

---
+++ LeetcodeCore/TextJustification.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class TextJustification
+    {
+        // 68. Text Justification
+        public IList<string> FullJustify(string[] words, int maxWidth)
+        {
+            var results = new List<string>();
+            var builder = new StringBuilder();
+            var linkedList = new LinkedList<string>();
+            var currCount = 0;
+            var i = 0;
+
+            while (true)
+            {
+                currCount += words[i].Length;
+                linkedList.AddLast(words[i]);
+                if (currCount > maxWidth - (linkedList.Count - 1))
+                {
+                    currCount -= words[i].Length;
+                    linkedList.RemoveLast();
+                    BuildWithCurrentWords(results, builder, linkedList, ref currCount, maxWidth);
+                }
+                else if (currCount == maxWidth - (linkedList.Count - 1))
+                {
+                    BuildWithCurrentWords(results, builder, linkedList, ref currCount, maxWidth);
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+
+                if (i == words.Length)
+                {
+                    if (linkedList.Count > 0) 
+                        BuildLastLine(results, builder, linkedList, ref currCount, maxWidth);
+                    break;
+                }
+            }
+
+            return results;
+        }
+
+        private void BuildWithCurrentWords(IList<string> results, StringBuilder builder, LinkedList<string> linkedList, ref int currCount, int maxWidth)
+        {
+            builder.Clear();
+            //
+            var arr = DivideSpaces(maxWidth - currCount, linkedList.Count - 1);
+            var i = 0;
+            while (i < arr.Length)
+            {
+                builder.Append(linkedList.First.Value);
+                linkedList.RemoveFirst();
+                builder.Append(new string(' ', arr[i]));
+                i++;
+            }
+            builder.Append(linkedList.First.Value);
+            builder.Append(new string(' ', maxWidth - builder.Length));
+            Console.WriteLine($"Length of this line: {builder.Length}");
+            results.Add(builder.ToString());
+            //
+            linkedList.Clear();
+            currCount = 0;
+        }
+
+        private void BuildLastLine(IList<string> results, StringBuilder builder, LinkedList<string> linkedList, ref int currCount, int maxWidth)
+        {
+            builder.Clear();
+            //
+            var arr = new int[linkedList.Count - 1];
+            Array.Fill(arr, 1);
+            var i = 0;
+            while (i < arr.Length)
+            {
+                builder.Append(linkedList.First.Value);
+                linkedList.RemoveFirst();
+                builder.Append(new string(' ', arr[i]));
+                i++;
+            }
+            builder.Append(linkedList.First.Value);
+            builder.Append(new string(' ', maxWidth - builder.Length));
+            Console.WriteLine($"Length of last line: {builder.Length}");
+            results.Add(builder.ToString());
+            //
+            linkedList.Clear();
+            currCount = 0;
+        }
+
+        private int[] DivideSpaces(int spaces, int slots)
+        {
+            if (slots > spaces)
+                throw new ArgumentOutOfRangeException();
+            if (slots == 0)
+                return new int[0];
+
+            var arr = new int[slots];
+            var i = 0;
+            while (spaces > 0)
+            {
+                arr[i]++;
+                i++;
+                if (i == arr.Length) i = 0;
+                spaces--;
+            }
+
+            return arr;
+        }
+    }
+}
