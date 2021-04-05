0b19f0db2f9c440bb872ab8c1325027cefe262fc
Yuhao Dong <ydong119@gmail.com>
2021-04-05 17:21:57 -0400

    Problem 205 IsomorphicStrings

---
+++ LeetcodeCore/IsomorphicStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class IsomorphicStrings
+    {
+        // 205. Isomorphic Strings
+        public bool IsIsomorphic(string s, string t)
+        {
+            var mapping1 = new int[256];
+            var mapping2 = new int[256];
+            Array.Fill(mapping1, -1);
+            Array.Fill(mapping2, -1);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (mapping1[(int)s[i]] == -1 && mapping2[(int)t[i]] == -1)
+                {
+                    mapping1[(int)s[i]] = (int)t[i];
+                    mapping2[(int)t[i]] = (int)s[i];
+                }
+                else if (mapping1[(int)s[i]] != (int)t[i] || mapping2[(int)t[i]] != (int)s[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
