2dd5bfb95c74e9c552bf660589e3e52e51ff5631
Yuhao Dong <ydong119@gmail.com>
2021-04-06 22:40:37 -0400

    Problem 261 GraphValidTree

---
+++ LeetcodeCore/GraphValidTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class GraphValidTree
+    {
+        // 261. Graph Valid Tree
+        // Ridiculously easy with UnionFind
+        public bool ValidTree(int n, int[][] edges)
+        {
+            var uf = new UnionFind(n);
+
+            foreach (var e in edges)
+            {
+                if (uf.Connected(e[0], e[1]))
+                    return false;
+
+                uf.Union(e[0], e[1]);
+            }
+
+            return uf.Count() == 1;
+        }
+    }
+}
