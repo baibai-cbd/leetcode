b04d68cf6e0a77117caaed57af975688dc08e124
Yuhao Dong <ydong119@gmail.com>
2021-04-07 22:47:06 -0400

    modification on using ValueTuple

--- LeetcodeCore/NumberOfDistinctIslands.cs
+++ LeetcodeCore/NumberOfDistinctIslands.cs
@@ -12,14 +12,14 @@ namespace LeetcodeCore
             var m = grid.Length;
             var n = grid[0].Length;
 
-            var pointHashSet = new HashSet<Tuple<int, int>>();
+            var pointHashSet = new HashSet<(int, int)>();
             var shapeHashSet = new HashSet<string>();
 
             for (int i = 0; i < m; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    if (grid[i][j] == 1 && !pointHashSet.Contains(new Tuple<int, int>(i,j)))
+                    if (grid[i][j] == 1 && !pointHashSet.Contains((i,j)))
                     {
                         var stringBuilder = new StringBuilder();
                         DFSFind(grid, m, n, pointHashSet, i, j, stringBuilder, '*');
@@ -32,16 +32,16 @@ namespace LeetcodeCore
             return shapeHashSet.Count;
         }
 
-        private void DFSFind(int[][] grid, int m, int n, HashSet<Tuple<int, int>> phs, int i, int j, StringBuilder sb, char direction)
+        private void DFSFind(int[][] grid, int m, int n, HashSet<(int, int)> phs, int i, int j, StringBuilder sb, char direction)
         {
             if (i < 0 || j < 0 || i >= m || j >= n)
                 return;
             if (grid[i][j] == 0)
                 return;
-            if (phs.Contains(new Tuple<int, int>(i, j)))
+            if (phs.Contains((i, j)))
                 return;
 
-            phs.Add(new Tuple<int, int>(i, j));
+            phs.Add((i, j));
             sb.Append(direction);
 
             DFSFind(grid, m, n, phs, i, j + 1, sb, 'D');
