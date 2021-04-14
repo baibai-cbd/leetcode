60735a1e91b0809f3a1d4f2608e774a7a234b4c1
Yuhao Dong <ydong119@gmail.com>
2021-04-13 23:57:09 -0400

    Problem 209 MinimumSizeSubarraySum

---
+++ LeetcodeCore/MinimumSizeSubarraySum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class MinimumSizeSubarraySum
+    {
+        // 209. Minimum Size Subarray Sum
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            var i = 0;
+            var j = 0;
+            var currSum = nums[0];
+            var minLength = nums.Length + 1;
+
+            for (i = 0; i < nums.Length; i++)
+            {
+                while (currSum < target && j < nums.Length - 1)
+                {
+                    j++;
+                    currSum += nums[j];
+                }
+
+                if (currSum >= target)
+                    minLength = Math.Min(minLength, j - i + 1);
+
+                currSum -= nums[i];
+            }
+
+            return minLength == nums.Length + 1 ? 0 : minLength;
+        }
+    }
+}
