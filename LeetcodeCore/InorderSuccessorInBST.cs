5d37e8f9315919ab5df2510a762bc7b87ec9b718
Yuhao Dong <ydong119@gmail.com>
2021-04-15 22:06:17 -0400

    Problem 285 InorderSuccessorInBST

---
+++ LeetcodeCore/InorderSuccessorInBST.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class InorderSuccessorInBST
+    {
+        // 285. Inorder Successor in BST
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            return SearchWithParent(null, root, p);
+        }
+
+        private TreeNode SearchWithParent(TreeNode parent, TreeNode root, TreeNode p)
+        {
+            if (root == p)
+            {
+                if (root.right == null)
+                {
+                    return parent;
+                }
+                else
+                {
+                    var nextNode = root.right;
+                    while (nextNode.left != null) nextNode = nextNode.left;
+                    return nextNode;
+                }
+            }
+            else if (root.val > p.val)
+            {
+                if (root.left != null) return SearchWithParent(root, root.left, p);
+                else return null;
+            }
+            else
+            {
+                if (root.right != null) return SearchWithParent(parent, root.right, p);
+                else return null;
+            }
+        }
+    }
+}
