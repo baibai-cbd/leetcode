ebce2476b7140d2b3adf24ca5fd549f788ad3c55
Yuhao Dong <ydong119@gmail.com>
2021-04-18 17:07:59 -0400

    Problem 64 MinimumPathSum

---
+++ LeetcodeCore/MinimumPathSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class MinimumPathSum
+    {
+        // 64. Minimum Path Sum
+        public int MinPathSum(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            var newGrid = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                newGrid[i] = new int[n];
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var leftSum = InGridRange(i, j - 1, m, n) ? newGrid[i][j - 1] : int.MaxValue;
+                    var topSum = InGridRange(i - 1, j, m, n) ? newGrid[i - 1][j] : int.MaxValue;
+                    if (leftSum == int.MaxValue && leftSum == topSum)
+                        newGrid[i][j] = grid[i][j];
+                    else
+                        newGrid[i][j] = grid[i][j] + Math.Min(leftSum, topSum);
+                }
+            }
+
+            return newGrid[m - 1][n - 1];
+        }
+
+        private bool InGridRange(int x, int y, int m, int n)
+            => x >= 0 && x < m && y >= 0 && y < n;
+    }
+}
