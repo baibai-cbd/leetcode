9d99b27cd5cc08b08e97d7198d381f42d21956a6
Yuhao Dong <ydong119@gmail.com>
2021-04-24 00:27:09 -0400

    Problem 1780 CheckIfNumberIsASumOfPowersOfThree

---
+++ LeetcodeCore/CheckIfNumberIsASumOfPowersOfThree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class CheckIfNumberIsASumOfPowersOfThree
+    {
+        // 1780. Check if Number is a Sum of Powers of Three
+        public bool CheckPowersOfThree(int n)
+        {
+            var powersOf3 = new int[15];
+
+            for (int i = 0, p = 1; i < 15; ++i, p *= 3)
+            {
+                powersOf3[i] = p;
+            }
+
+            for (int i = 14; i >= 0 && n > 0; --i)
+            {
+                if (n >= powersOf3[i])
+                {
+                    n -= powersOf3[i];
+                }
+            }
+            return n == 0;
+        }
+    }
+}
