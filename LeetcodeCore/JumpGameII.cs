4b71f797c8c49f6c6a7d90e6bcc5a3beec77f087
Yuhao Dong <ydong119@gmail.com>
2021-06-06 21:54:59 -0400

    Problem 45 JumpGameII

---
+++ LeetcodeCore/JumpGameII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class JumpGameII
+    {
+        // 45. Jump Game II
+        // Implicit BFS solution
+        // traverse thru currRange is BFS on the current "level", increment count and update range goes to the next level
+        public int Jump(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+
+            var currIndex = 0;
+            var currRange = 0;
+            var nextRange = 0;
+            var count = 0;
+
+            while (true)
+            {
+                for (int i = currIndex; i <= currRange; i++)
+                {
+                    nextRange = Math.Max(nextRange, i + nums[i]);
+                }
+
+                count++;
+
+                if (nextRange >= nums.Length - 1)
+                    return count;
+
+                currIndex = currRange + 1;
+                currRange = nextRange;
+                nextRange = 0;
+            }
+        }
+    }
+}
