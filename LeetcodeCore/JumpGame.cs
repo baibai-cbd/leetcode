d3f223acf57b835767bd4d4ccf88fcf8b21db704
Yuhao Dong <ydong119@gmail.com>
2021-06-06 21:43:39 -0400

    Problem 55 JumpGame

---
+++ LeetcodeCore/JumpGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class JumpGame
+    {
+        // 55. Jump Game
+        public bool CanJump(int[] nums)
+        {
+            var currIndex = 0;
+            var currRange = Math.Min(nums.Length - 1, nums[0]);
+            var nextRange = 0;
+
+            while (true)
+            {
+                for (int i = currIndex; i <= currRange; i++)
+                {
+                    nextRange = Math.Max(nextRange, i + nums[i]);
+                }
+
+                if (nextRange >= nums.Length - 1)
+                    return true;
+                if (nextRange == currRange)
+                    return false;
+
+                currIndex = currRange;
+                currRange = nextRange;
+                nextRange = 0;
+            }
+        }
+    }
+}
