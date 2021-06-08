045e4941e13ba18c053a95afd7fbb9ae381dec97
Yuhao Dong <ydong119@gmail.com>
2021-06-07 22:02:43 -0400

    Problem 1306 JumpGameIII

---
+++ LeetcodeCore/JumpGameIII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class JumpGameIII
+    {
+        // 1306. Jump Game III
+        public bool CanReach(int[] arr, int start)
+        {
+            var visited = new bool[arr.Length];
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var curr = queue.Dequeue();
+                visited[curr] = true;
+
+                if (arr[curr] == 0)
+                    return true;
+
+                var p1 = curr - arr[curr];
+                var p2 = curr + arr[curr];
+
+                if (p1 >= 0 && !visited[p1])
+                    queue.Enqueue(p1);
+                if (p2 < arr.Length && !visited[p2])
+                    queue.Enqueue(p2);
+            }
+
+            return false;
+        }
+    }
+}
