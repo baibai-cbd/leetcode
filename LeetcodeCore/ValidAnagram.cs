cf6fc15a890a8e3fc1e32d7b580d92183f7969d6
Yuhao Dong <ydong119@gmail.com>
2021-06-09 22:38:06 -0400

    Problem 242 ValidAnagram

---
+++ LeetcodeCore/ValidAnagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeCore
+{
+    public class ValidAnagram
+    {
+        // 242. Valid Anagram
+        public bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length)
+                return false;
+
+            var arr = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[s[i] - 'a']++;
+                arr[t[i] - 'a']--;
+            }
+
+            return arr.All(x => x == 0);
+        }
+    }
+}
